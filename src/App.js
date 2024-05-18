f611ef0b96b6d970df77104d2e25776a5720689b
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2024-05-18 16:04:34 -0300

    feat: criando useSnae para valida o login e usando a pagina de login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/header";
 import ListarTarefa from "./pages/tarefa/ListarTarefa";
+import PaginaLogin from "./pages/login/PaginaLogin";
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   useEffect(() => {
     document.title = "Gerenciador de Tarefas";
   }, []);
 
+  if (isAuthenticated) {
+    return (
+      <div className="App">
+        <Header />
+        <ListarTarefa />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <Header />
-      <ListarTarefa />
+      <PaginaLogin isAuthenticated={setIsAuthenticated}/>
     </div>
   );
 }
